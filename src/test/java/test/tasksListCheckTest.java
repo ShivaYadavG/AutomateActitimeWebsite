dd76287dcbeb66f9789c2b86f42fd318db44ada5
ShivYadavG <shivayadavg44@gmail.com>
2024-05-28 11:32:21 +0530

    Second commit

--- src/test/java/test/tasksListCheckTest.java
+++ src/test/java/test/tasksListCheckTest.java
@@ -37,6 +37,7 @@ public class tasksListCheckTest extends BaseActi {
 	public void tasksListCheckTest() throws InterruptedException {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		Actions actions = new Actions(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		tasksListCheckTestObjects tasksListCheckTestObjects = new tasksListCheckTestObjects(driver);
 		LogInPage LogInPage = new LogInPage(driver);
 		ElementSelector ElementSelector = new ElementSelector(driver);
@@ -58,32 +59,30 @@ public class tasksListCheckTest extends BaseActi {
 		// clicking on Big bang company and getting all tasks list
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(tasksListCheckTestObjects.BigBangCompanyCust())));
 		ElementSelector.clickByxpath(tasksListCheckTestObjects.BigBangCompanyCust());
-		Thread.sleep(5000);
 		HashMap<String, List<String>> BigBangCompanyHM = gettingTasksList("BigBangCompanyCust");
 
 		// clicking on Flight operation project and getting all tasks list
 		wait.until(
 				ExpectedConditions.elementToBeClickable(By.xpath(tasksListCheckTestObjects.FlightOperationProject())));
-		ElementSelector.clickByxpath(tasksListCheckTestObjects.FlightOperationProject());
-		Thread.sleep(3000);
+		WebElement flightOperationCustomer = driver
+				.findElement(By.xpath(tasksListCheckTestObjects.FlightOperationProject()));
+		js.executeScript("arguments[0].click()", flightOperationCustomer);
 		HashMap<String, List<String>> FlightOperationProjectHM = gettingTasksList("FlightOperationProject");
 
 		// clicking on Space ship building project and getting all tasks list
 		wait.until(ExpectedConditions
 				.elementToBeClickable(By.xpath(tasksListCheckTestObjects.SpaceShipBuildingProject())));
-		ElementSelector.clickByxpath(tasksListCheckTestObjects.SpaceShipBuildingProject());
-		Thread.sleep(3000);
+		WebElement spaceShipBuildin = driver
+				.findElement(By.xpath(tasksListCheckTestObjects.SpaceShipBuildingProject()));
+		js.executeScript("arguments[0].click()", spaceShipBuildin);
 		HashMap<String, List<String>> ShipBuildingOperationHM = gettingTasksList("SpaceShipBuildingProject");
 
 		// clicking on Space ship testing project and getting all tasks list
 		wait.until(
 				ExpectedConditions.elementToBeClickable(By.xpath(tasksListCheckTestObjects.SpaceShipTestingProject())));
-		ElementSelector.clickByxpath(tasksListCheckTestObjects.SpaceShipTestingProject());
-		Thread.sleep(3000);
+		WebElement spaceShipTesting = driver.findElement(By.xpath(tasksListCheckTestObjects.SpaceShipTestingProject()));
+		js.executeScript("arguments[0].click()", spaceShipTesting);
 		HashMap<String, List<String>> ShipTestingProjectHM = gettingTasksList("SpaceShipTestingProject");
-
-		// Calllin mehtod to print all the tasks in big bang compay
-		printTasks(BigBangCompanyHM);
 
 		// calling methods to verify the whether the tasks are present in the Big bang
 		areTasksPresentInBigBangCust(BigBangCompanyHM, FlightOperationProjectHM);
@@ -102,65 +101,60 @@ public class tasksListCheckTest extends BaseActi {
 		log.debug("Total number of tasks are verified");
 	}
 
-	// Getting all tasks in Big bang company customer and its projects
-	public HashMap gettingTasksList(String project_CustomerName) throws InterruptedException {
+	public HashMap<String, List<String>> gettingTasksList(String project_CustomerName) throws InterruptedException {
 		tasksListCheckTestObjects tasksListCheckTestObjects = new tasksListCheckTestObjects(driver);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		Actions actions = new Actions(driver);
+
 		HashMap<String, List<String>> gettingTasksList = new HashMap<>();
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("document.body.style.zoom='40%'");
-		
-		String StatusXpath = "//*[text()[contains(.,'OPEN TASKS')]]//following::*[@class='name gradientEllipsis']";
-		
-		WebElement flightOperation = driver.findElement(By.xpath("//*[text()='OPEN TASKS']//following::*[@class='projectName' and text()='Flight operations']/..//div[@class='icon']"));
-		js.executeScript("arguments[0].click();", flightOperation);
-		Thread.sleep(1000);
-		List<WebElement> list =driver.findElements(By.xpath(StatusXpath));
-		for(WebElement ele : list) {
-			System.out.println(ele.getText());
-		}
-		WebElement spaceBuilding = driver.findElement(By.xpath("//*[text()='OPEN TASKS']//following::*[@class='projectName' and text()='Spaceship building']/..//div[@class='icon']"));
-		js.executeScript("arguments[0].click();", spaceBuilding);
-		Thread.sleep(1000);
-		List<WebElement> list2 =driver.findElements(By.xpath(StatusXpath));
-		for(WebElement ele : list2) {
-			System.out.println(ele.getText());
-		}
-		WebElement spaceTesting = driver.findElement(By.xpath("//*[text()='OPEN TASKS']//following::*[@class='projectName' and text()='Spaceship testing']/..//div[@class='icon']"));
-		js.executeScript("arguments[0].click();", spaceTesting);
-		Thread.sleep(1000);
-		List<WebElement> list3 =driver.findElements(By.xpath(StatusXpath));
-		for(WebElement ele : list3) {
-			System.out.println(ele.getText());
-		}
-	
-		List<WebElement> minimizeMaximizeIcons = driver.findElements(By.xpath(tasksListCheckTestObjects.minimizeCustomer()));
-		List<WebElement> tasksOfCustomerList = driver.findElements(By.xpath(tasksListCheckTestObjects.tasksInCustomersRow()));
-		for (WebElement customerMaxMinIcon : minimizeMaximizeIcons) {
-			for (WebElement taskOfCutomer : tasksOfCustomerList) {
-				wait.until(ExpectedConditions.visibilityOf(taskOfCutomer));
-				tasks.add(taskOfCutomer.getText());
-				System.out.println(taskOfCutomer.getText());
+		js.executeScript("document.body.style.zoom='60%'");
+
+		if (project_CustomerName.equalsIgnoreCase("BigBangCompanyCust")) {
+			String[] projects = { "Flight operations", "Spaceship building", "Spaceship testing" };
+			List<WebElement> minimizeMaximizeIcons = driver
+					.findElements(By.xpath(tasksListCheckTestObjects.minimizeCustomer()));
+
+			for (String project : projects) {
+				List<String> tasks = new ArrayList<>(); // Create a new list for each customer
+
+				// Find all tasks for the customer
+				Thread.sleep(3000);
+				List<WebElement> tasksOfCustomerList = driver
+						.findElements(By.xpath(tasksListCheckTestObjects.tasksInCustomersRow()));
+				for (WebElement taskOfCustomer : tasksOfCustomerList) {
+					wait.until(ExpectedConditions.visibilityOf(taskOfCustomer));
+					tasks.add(taskOfCustomer.getText());
+				}
+
+				// Add the tasks for the current project to the map
+				gettingTasksList.put(project, tasks);
+
+				// Minimize the project if necessary
+				String customerName = "//*[text()='OPEN TASKS']//following::*[@class='projectName' and text()='"
+						+ project + "']";
+				WebElement minimize = driver.findElement(By.xpath(customerName));
+				wait.until(ExpectedConditions.elementToBeClickable(minimize));
+				js.executeScript("arguments[0].click();", minimize);
 			}
-			customerMaxMinIcon.click();
+		} 
+		else {
+			List<String> tasks = new ArrayList<>(); // Create a new list for the projects of customer
+
+			// Find all tasks in the project of customer
+			Thread.sleep(3000);
+			List<WebElement> tasksOfCustomerList = driver
+					.findElements(By.xpath(tasksListCheckTestObjects.tasksInCustomersRow()));
+			for (WebElement taskOfCustomer : tasksOfCustomerList) {
+				wait.until(ExpectedConditions.visibilityOf(taskOfCustomer));
+				tasks.add(taskOfCustomer.getText());
+			}
+
+			// Add the tasks for the customer to the map
+			gettingTasksList.put(project_CustomerName, tasks);
 		}
-		
-		
-		// Add tasksList to the hash map
-		gettingTasksList.put(project_CustomerName, tasks);
+
 		return gettingTasksList;
-	}
-
-	// Adding tasks in tasks list
-	public void addingTasksInHashmapList(String headerXpath) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-		List<WebElement> inNewTabTasksList = driver.findElements(By.xpath(headerXpath));
-		for (WebElement inNewTabTask : inNewTabTasksList) {
-			wait.until(ExpectedConditions.visibilityOf(inNewTabTask));
-			tasks.add(inNewTabTask.getText());
-		}
-
 	}
 
 	// Total number of tasks from a HashMap
@@ -172,15 +166,6 @@ public class tasksListCheckTest extends BaseActi {
 		return totalTasks;
 	}
 
-	// printing big bang company tasks for manual verification
-	public void printTasks(HashMap<String, List<String>> BigBangCompanyHM) {
-		for (Map.Entry<String, List<String>> tasks : BigBangCompanyHM.entrySet()) {
-			for (List<String> task : BigBangCompanyHM.values()) {
-				System.out.println("Big bang company tasks : " + task);
-			}
-		}
-	}
-
 	// Validations of tasks present in the Big Bang customer
 	public void areTasksPresentInBigBangCust(HashMap<String, List<String>> BigBangCompanyHM,
 			HashMap<String, List<String>> FlightOperationProjectHM) {
@@ -189,7 +174,6 @@ public class tasksListCheckTest extends BaseActi {
 				boolean taskFound = false;
 				for (List<String> bigBangTasks : BigBangCompanyHM.values()) {
 					if (bigBangTasks.contains(flightTask)) {
-						System.out.println(flightTask + " is present");
 						taskFound = true;
 						break;
 					}
